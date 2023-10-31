bba166630582668d1eb2cb45ee15e5ccd6933156
Chris Wininger <chris.wininger@gmail.com>
2023-10-31 13:02:51 -0400

    start on boot

--- app/start.sh
+++ app/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # runs on the pi where ansible has created a virtual env and copied the app script into the same directory
-source ./venv/bin/activate
-python3 ./app.py --no-output
+source /home/chris/ansible-pi-halloween-motion-app/venv/bin/activate
+python3 /home/chris/ansible-pi-halloween-motion-app/app.py --no-output
